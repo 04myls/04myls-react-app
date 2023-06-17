c3117cf48106cb46fd8ff6c47b5c2fabfa5101c9
04myls <04myls@MacBook-Pro.local>
2023-06-17 09:47:33 +0900

    ボタン・入力欄完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Title from './components/Title';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="test">
      <Title/>
+     <Form/>
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = () => {
+  return (
+<form>
+<input type="text" name="city" placeholder="都市名" />
+    <button type="submit">Get Weather</button>
+</form>
+  )
+}
+
+export default Form
