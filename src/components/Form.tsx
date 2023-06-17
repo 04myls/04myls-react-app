04c845f710e9591926b0936b7bd01878db581a76
04myls <04myls@MacBook-Pro.local>
2023-06-17 09:58:19 +0900

    state作成

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
 const Form = () => {
+    const[city, setCity] = useState('')
   return (
 <form>
-<input type="text" name="city" placeholder="都市名" />
+<input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)
+} />
+{city}
     <button type="submit">Get Weather</button>
 </form>
   )
