0be1e81ab04ca213cb4ff9b6877b4b22b3a4bf94
04myls <04myls@gmail.com>
2023-06-17 10:23:20 +0900

    fix

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react"
 const Form = () => {
-    const[city, setCity] = useState('')
+    const[city, setCity] = useState<String>('')
   return (
 <form>
-<input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)
+<input type="text" name="city" placeholder="テスト" onChange={e =>setCity(e.target.value)
 } />
 {city}
     <button type="submit">Get Weather</button>
 </form>
   )
 }
-
 export default Form
