b6dc3ddce077f43348867d48e515071def012de4
04myls <04myls@gmail.com>
2023-06-17 10:28:29 +0900

    fix

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,7 @@ const Form = () => {
     const[city, setCity] = useState<String>('')
   return (
 <form>
-<input type="text" name="city" placeholder="テスト" onChange={e =>setCity(e.target.value)
+<input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)
 } />
 {city}
     <button type="submit">Get Weather</button>
