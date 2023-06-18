0cc4d773f1ca2cd8122e1083640791aa1d1ac653
04myls <04myls@gmail.com>
2023-06-18 20:18:30 +0900

    API接続

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
-import Title from './components/Title';
-import Form from './components/Form';
+import { useState } from 'react';
+import Title from "./components/Title";
+import Form from "./components/Form"; 
+import Results from "./components/Results";
+import Loading from "./components/Loading";
 import './App.css';
 
+type ResultsStateType = {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string; 
+    icon: string;
+}
+
 function App() {
-  return (
-    <div className="test">
-     <Title/>
-     <Form/>
-    </div>
-  );
+    const [loading, setLoading] = useState<boolean>(false);
+    const [city, setCity] = useState<string>("");
+    const [results, setResults] = useState<ResultsStateType>({ 
+        country: "",
+        cityName: "",
+        temperature: "", 
+        conditionText: "",
+        icon: ""
+    });
+    const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        fetch("http://api.weatherapi.com/v1/current.json?key=494421d7ee54448895613806231706&q=London&aqi=no")
+            .then(res => res.json())
+            .then(data => {
+                setResults({
+                    country: data.location.country,
+                    cityName: data.location.name,
+                    temperature: data.current.temp_c,
+                    conditionText: data.current.condition.text,
+                    icon: data.current.condition.icon
+                })
+                setCity("");
+                setLoading(false);
+            })
+            .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"))
+    }
+    return (
+      <div className="wrapper">
+          <div className="container">
+              <Title />
+              <Form getWeather={getWeather} setCity={setCity} city={city} /> 
+              {loading ? <Loading /> : <Results results={results} />}   
+          </div>
+      </div>
+    );
 }
 
 export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
-const Form = () => {
-    const[city, setCity] = useState<String>('')
-  return (
-<form>
-<input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)
-} />
-{city}
-    <button type="submit">Get Weather</button>
-</form>
-  )
+type FormPropsType = {
+  city: string;
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
 }
-export default Form
+
+const Form = ({ city, setCity, getWeather }: FormPropsType) => {
+  return (
+      <form onSubmit={getWeather}>
+          <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city}/> 
+          <button type="submit">Get Weather</button> 
+      </form>
+  );
+};
+
+export default Form;
